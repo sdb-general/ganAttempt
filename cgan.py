736ea48b572c777dde67cd169f51412c4f5f4e8d
stke9 <sk3619@ic.ac.uk>
2021-02-27 16:39:39 +0000

    fix optimiser

--- cgan.py
+++ cgan.py
@@ -25,26 +25,27 @@ mypath = './torchtest.pth'
 # plt.imshow(train[0])
 # plt.show()
 
-device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
 
 batch_size = 50
 image_size = 28
 nc = 1 #number of channels, these are single channel images
 nz = 100 #size of latent z vector
-ngf = 16 #size of feature maps in the generator, assuming must be same as image size (edit: this was wrong)
-ndf = 16 #same but discrim #change back to 28 if using first versin of discrim
+ngf = 64 #size of feature maps in the generator, assuming must be same as image size (edit: this was wrong)
+ndf = 64 #same but discrim #change back to 28 if using first versin of discrim
 num_epochs = 25
 lr = 0.0002 #learnig rate
+lrg = 0.001
+d_iters = 5
 beta1 = 0.5 #some para i don't understand
-ngpu = 0 #cpu mode
+ngpu = 1 #cpu mode
 
 mnist_trainset = dset.MNIST(root='./data', train=True, download=True, transform=None)
-train_data = mnist_trainset.train_data
+train_data = mnist_trainset.data
 dataset = torch.utils.data.TensorDataset(train_data/256, mnist_trainset.train_labels)
 dataloader = torch.utils.data.DataLoader(dataset,
 										batch_size=batch_size,
-										shuffle=True,
-										num_workers=0) #workers is number of threads for the dataloader, unsure of function
+										shuffle=True) #workers is number of threads for the dataloader, unsure of function
 
 
 
@@ -75,7 +76,6 @@ class Generator(nn.Module):
 	def __init__(self, ngpu):
 		super(Generator, self).__init__()
 		self.label_emb = nn.Embedding(10, 10) #second arg could be something else
-		self.ngpu = ngpu
 		self.main = nn.Sequential(
 			nn.ConvTranspose2d(nz + 10, ngf * 8, 4, 1, 0, bias = False), #(input channels, outoput chanenls, size of kernel, stride length, padding = cutoff at end of otput...)
 			nn.BatchNorm2d(ngf * 8),
@@ -144,9 +144,9 @@ netG.apply(weights_init)
 
 
 # print(netD)
-y = torch.randn(1, 1, 28, 28)
-x = netD(y, torch.randint(10,(1,)))
-# print(x.squeeze())
+# y = torch.randn(1, 1, 28, 28)
+# x = netD(y, torch.randint(10,(1,)))
+# # print(x.squeeze())
 # raise ValueError
 
 
@@ -156,11 +156,11 @@ x = netD(y, torch.randint(10,(1,)))
 
 #########################################
 
-criterion = nn.BCELoss()
+criterion = nn.MSELoss()
 
 #create batch of latent vectors
 
-fixed_noise = torch.randn(64, nz, 1, 1, device = device)
+fixed_noise = torch.randn(50, nz, 1, 1, device=device)
 
 #conventions
 
@@ -170,7 +170,7 @@ fake_label = 0.
 #setup adam optimisers for G and D
 
 optimiserD = optim.Adam(netD.parameters(), lr=lr, betas= (beta1, 0.999))
-optimiserG = optim.Adam(netD.parameters(), lr=lr, betas= (beta1, 0.999))
+optimiserG = optim.Adam(netG.parameters(), lr=lrg, betas= (beta1, 0.999))
 
 ########################################
 
@@ -187,7 +187,7 @@ G_losses = []
 D_losses = []
 iters = 0
 
-
+print(device)
 def train():
 	print('starting the training loop . ... ... . .')
 	for epoch in range(num_epochs):
@@ -202,6 +202,7 @@ def train():
 
 			netD.zero_grad()
 			real_cpu = data.to(device).unsqueeze(1).float()
+			num_label = num_label.to(device)
 			#print(real_cpu.shape)
 			b_size = real_cpu.size(0)
 			label = torch.full((b_size,), 
@@ -243,27 +244,29 @@ def train():
 			##################################
 			#update G network
 			##################################
-			netG.zero_grad()
-			label.fill_(real_label) #fake labels are real for generator cost
+			if index % d_iters == 0:
 
-			#we just updated D, so we do another forward pass of the fake batch through D
-			output = netD(fake, num_label).view(-1)
-			#calc G's loss
-			errG = criterion(output, label)
-			#calc gradients
-			errG.backward()
-			D_G_Z2 = output.mean().item()
-			#update G
-			optimiserG.step()
+				netG.zero_grad()
+				label.fill_(real_label) #fake labels are real for generator cost
+
+				#we just updated D, so we do another forward pass of the fake batch through D
+				output = netD(fake, num_label).view(-1)
+				#calc G's loss
+				errG = criterion(output, label)
+				#calc gradients
+				errG.backward()
+				D_G_Z2 = output.mean().item()
+				#update G
+				optimiserG.step()
 
 			#output training stats
 			if index % 50 == 0:
 				print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
 	                  % (epoch, num_epochs, index, len(dataloader),
 	                     errD.item(), errG.item(), D_x, D_G_Z1, D_G_Z2))
-				x = netG(torch.randn(1, nz, 1, 1),torch.randint(9,(1,)))
+				x = netG(torch.randn(1, nz, 1, 1).to(device), torch.randint(9, (1,)).to(device))
 				plt.figure()
-				plt.imshow(x.detach().numpy().squeeze(0).squeeze(0))
+				plt.imshow(x.detach().cpu().numpy().squeeze(0).squeeze(0))
 				plt.savefig('epochNumber{}'.format(epoch + 1))
 				plt.close()
 			G_losses.append(errG.item())
@@ -274,8 +277,8 @@ def train():
 			#check how the gen is doing by saving outputs on fixed_noise
 
 			#skipped this part
-	#torch.save(netD.state_dict(), mypath)
-	torch.save(netG.state_dict(), mypath)
+	# torch.save(netD.state_dict(), mypath)
+	# torch.save(netG.state_dict(), mypath)
 
 
 
